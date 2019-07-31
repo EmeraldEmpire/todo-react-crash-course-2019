3fff2aa13e0664663e4788b7f55c047c4e694334
Eko <emeraldwhite@gmail.com>
2019-07-31 17:51:13 +0800

    ome Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
