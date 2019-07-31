b0ce39c9dfea7843efe6a02ecaef629acf60c4ca
Eko <emeraldwhite@gmail.com>
2019-07-31 17:14:49 +0800

    Updates

---
+++ precache-manifest.bd20ba0637082d166f3d2c44678f5cbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d6082fd4855878c9a07887029b871e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42b15557199e0ef999df",
+    "url": "/static/css/main.9c645cde.chunk.css"
+  },
+  {
+    "revision": "28be84e997f65e60f504",
+    "url": "/static/js/2.92dbae0f.chunk.js"
+  },
+  {
+    "revision": "42b15557199e0ef999df",
+    "url": "/static/js/main.6001ccb5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
